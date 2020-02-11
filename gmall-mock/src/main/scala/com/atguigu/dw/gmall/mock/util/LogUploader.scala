e602d0ae077e534c6b290f7388c9acd98364409d
John Doe <30752740@qq.com>
2020-02-11 11:37:23 +0800

    更新

--- gmall-mock/src/main/scala/com/atguigu/dw/gmall/mock/util/LogUploader.scala
+++ gmall-mock/src/main/scala/com/atguigu/dw/gmall/mock/util/LogUploader.scala
@@ -12,8 +12,8 @@ object LogUploader {
     /*发送日志*/
     def sendLog(log: String): Unit = {
         try {
-            // 1. 日志服务器的地址
-            val logUrl = new URL("http://localhost:8080/log")
+            // 1. 日志服务器的地址 发送的是80端口, 则端口号可以省略
+            val logUrl = new URL("http://hadoop102:80/log")
             // 2. 得到一个 HttpURLConnection
             val conn: HttpURLConnection = logUrl.openConnection().asInstanceOf[HttpURLConnection]
             // 3. 设置请求方法(上传数据一般使用 post 请求)   get post
---
+++ src/main/resources/all_log.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+gmall0830=/opt/gmall0830
+case $1 in
+start)
+    for host in hadoop102 hadoop103 hadoop104 ; do
+        ssh $host "source /etc/profile ; nohup java -jar $gmall0830/gmall-logger-0.0.1-SNAPSHOT.jar 1>>$gmall0830/gmall.log  2>>gmall.err &"
+    done
+
+;;
+stop)
+    for host in hadoop102 hadoop103 hadoop104 ; do
+        ssh $host "source /etc/profile ; jps | grep gmall-logger-0.0.1-SNAPSHOT.jar | awk {'print \$1'} | xargs kill -9"
+    done
+;;
+help)
+    echo "启动日志服务: "
+    echo "    log_all start"
+    echo "停止日志服务: "
+    echo "    log_all stop"
+;;
+* )
+    echo "你脚本使用的姿势不对,换个姿势再来"
+    echo "启动日志服务: "
+    echo "    log_all start"
+    echo "停止日志服务: "
+    echo "    log_all stop"
+;;
+esac
+
+
+# jps | grep gmall-logger-0.0.1-SNAPSHOT.jar | awk {'print $1'} | xargs kill -9
+# jps | awk '/gmall-logger-0.0.1-SNAPSHOT.jar/ {print $1}' | xargs -9 kill
